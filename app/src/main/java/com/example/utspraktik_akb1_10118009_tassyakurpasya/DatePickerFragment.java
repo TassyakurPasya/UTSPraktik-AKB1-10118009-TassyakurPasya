50f0a98c839fca120028bd28506fe2e096877fdd
Achapasya2109 <acha.pasya2109@gmail.com>
2021-06-05 12:49:57 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_akb1_10118009_tassyakurpasya/DatePickerFragment.java
@@ -0,0 +1,4 @@
+package com.example.utspraktik_akb1_10118009_tassyakurpasya;
+
+public class DatePickerFragment {
+}
---
+++ app/src/main/java/com/example/utspraktik_akb1_10118009_tassyakurpasya/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.utspraktik_akb1_10118009_tassyakurpasya;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+
+    Calendar myCalendar;
+    DatePickerDialog.OnDateSetListener date;
+
+    TextView datedeparture;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        datedeparture = findViewById(R.id.edittanggal);
+
+        myCalendar = Calendar.getInstance();
+        date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                TextView tanggal = findViewById(R.id.edittanggal);
+                String myFormat = "dd-MMMM-yyyy";
+                SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+                tanggal.setText(sdf.format(myCalendar.getTime()));
+            }
+        };
+
+        datedeparture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(MainActivity.this, date,
+                        myCalendar.get(Calendar.YEAR),
+                        myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
+    }
+}
